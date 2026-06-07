5ed9fd78435a1382c39836e3b9a965bca6b32c82
martinSadir21 <martin.sadir21@gmail.com>
2026-06-07 19:25:08 -0300

    Actividad completa

--- src/server.js
+++ src/server.js
@@ -16,4 +16,32 @@ const usuarios = [
 
 // TODO: Define tus rutas aquí
 
+app.get('/', (req, res) => {
+  res.send('Bienvenid@s a nuestro servidor Express!');
+});
+
+app.get('/productos', (req, res) => {
+  const { categoria } = req.query;
+  
+  if (categoria) {
+    const filtrados = productos.filter(p => p.categoria === categoria);
+    return res.json(filtrados);
+  }
+  
+  res.json(productos);
+});
+
+app.get('/usuarios/:id', (req, res) => {
+  const usuario = usuarios.find(u => u.id === parseInt(req.params.id)); 
+  
+  if (!usuario) {
+    return res.status(404).send('Usuario no encontrado.'); 
+  }
+  
+  res.json(usuario);
+});
+
+app.use((req, res) => {
+  res.status(404).send('No se ha encontrado la ruta ingresada.');
+});
 module.exports = app;
